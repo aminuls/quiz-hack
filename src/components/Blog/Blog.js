4135a48f2f6681a1b5500d2b03732823dff1a7a3
aminul <accounreate@gmail.com>
2022-10-20 01:01:41 +0600

    question3 added

--- src/components/Blog/Blog.js
+++ src/components/Blog/Blog.js
@@ -15,6 +15,14 @@ const Blog = () => {
                <p>The React Context API is a way for a React app to effectively produce global variables that can be passed around. This is the alternative to "prop drilling" or moving props from grandparent to child to parent, and so on. Context is also touted as an easier, lighter approach to state management using Redux.</p>
             </div>
          </div>
+         <div tabIndex={0} className="collapse collapse-arrow border-4 bg-base-100 rounded">
+            <div className="collapse-title text-xl font-medium">Tell us something about useRef?</div>
+            <div className="collapse-content font-semibold">
+               <p>The useRef Hook allows you to persist values between renders. It can be used to store a mutable value that does not cause a re-render when updated. It can be used to access a DOM element directly.
+               If we tried to count how many times our application renders using the useState Hook, we would be caught in an infinite loop since this Hook itself causes a re-render. To avoid this, we can use the useRef Hook.
+               </p>
+            </div>
+         </div>
       </div>
    );
 };
