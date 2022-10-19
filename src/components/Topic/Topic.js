d31b00c1059f35704f9c3ac2c74b5de700b86dcf
aminul <accounreate@gmail.com>
2022-10-19 22:38:01 +0600

    quiz completed

--- src/components/Topic/Topic.js
+++ src/components/Topic/Topic.js
@@ -1,9 +1,54 @@
-import React from 'react';
+import React from "react";
+import { useLoaderData } from "react-router-dom";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 const Topic = () => {
+   const quizzes = useLoaderData().data;
+   const { name, questions } = quizzes;
+   // console.log(quizzes);
+
+   const statusHandler = (option, correctAnswer, id) => {
+      const quizOption = document.getElementById(id);
+      if (option === correctAnswer) {
+         const notify = () => toast.success("Wow ! Your Answer is Correct");
+         quizOption.classList.add("bg-green-400");
+         notify();
+      } else {
+         const notify = () => toast.error("Oops ! You Answer is Wrong");
+         quizOption.classList.add("bg-red-400");
+         notify();
+      }
+   };
+
    return (
-      <div>
-         <h3>This is quiz page</h3>
+      <div className="mt-4">
+         <ToastContainer position="top-center"/>
+         <h2 className="text-2xl font-semibold">Quiz of {name}</h2>
+         <div className="flex flex-wrap flex-col items-center gap-5 mt-8">
+            {questions.map((quiz, index) => {
+               const { question, correctAnswer, id, options } = quiz;
+               // console.log(correctAnswer);
+               return (
+                  <div key={index} className="card mx-3 md:mx-0 sm:w-full md:w-4/5 lg:w-3/5 shadow-2xl">
+                     <div className="card-body p-2 py-4 sm:p-8 flex items-center">
+                        <h2 className="card-title">{`Quiz ${index + 1}: ${question}`}</h2>
+                        <div className="grid grid-cols-1 sm:grid-cols-2 gap-5 w-full justify-center mt-5">
+                           {options.map((option, idx) => {
+                              const generateId = `id${idx}${index}${id}`;
+                              console.log(generateId);
+                              return (
+                                 <div key={idx} id={generateId} className="border-2 border-sky-700 rounded-md h-20 flex items-center cursor-pointer" onClick={() => statusHandler(option, correctAnswer, generateId)}>
+                                    <p className="font-semibold px-2 text-lg">{option}</p>
+                                 </div>
+                              );
+                           })}
+                        </div>
+                     </div>
+                  </div>
+               );
+            })}
+         </div>
       </div>
    );
 };
