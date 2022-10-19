56f9362133c53d6fe8ee28c56a68125c611ccdbf
aminul <accounreate@gmail.com>
2022-10-20 00:26:33 +0600

    Bar chart added

--- src/components/Statistics/Statistics.js
+++ src/components/Statistics/Statistics.js
@@ -1,11 +1,22 @@
-import React from 'react';
+import React from "react";
+import { useLoaderData } from "react-router-dom";
+import { BarChart, Bar, ResponsiveContainer, CartesianGrid, XAxis, YAxis, Tooltip, Legend } from "recharts";
 
-const Statistics = () => {
+function Statistics() {
+   const data = useLoaderData().data;
    return (
-      <div>
-         <h2>This is Statistic page</h2>
+      <div className="h-96 lg:w-2/5 mx-auto my-auto">
+         <ResponsiveContainer width="100%" height="100%" className="flex pt-20">
+            <BarChart width={450} height={400} data={data}>
+               <CartesianGrid strokeDasharray="3 3" />
+               <XAxis dataKey="name" />
+               <YAxis tickCount={7} />
+               <Tooltip />
+               <Legend />
+               <Bar dataKey="total" fill="#8884d8" />
+            </BarChart>
+         </ResponsiveContainer>
       </div>
    );
-};
-
+}
 export default Statistics;
--- src/components/Topic/Topic.js
+++ src/components/Topic/Topic.js
@@ -1,3 +1,5 @@
+import { faEye } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import React from "react";
 import { useLoaderData } from "react-router-dom";
 import { ToastContainer, toast } from "react-toastify";
@@ -21,9 +23,14 @@ const Topic = () => {
       }
    };
 
+   const visibilityHandler = (answer) => {
+      const notify = () => toast.success(`The Correct answer is: ${answer}`);
+      notify();
+   };
+
    return (
       <div className="mt-4">
-         <ToastContainer position="top-center"/>
+         <ToastContainer position="top-center" />
          <h2 className="text-2xl font-semibold">Quiz of {name}</h2>
          <div className="flex flex-wrap flex-col items-center gap-5 mt-8">
             {questions.map((quiz, index) => {
@@ -32,7 +39,12 @@ const Topic = () => {
                return (
                   <div key={index} className="card mx-3 md:mx-0 sm:w-full md:w-4/5 lg:w-3/5 shadow-2xl">
                      <div className="card-body p-2 py-4 sm:p-8 flex items-center">
-                        <h2 className="card-title">{`Quiz ${index + 1}: ${question}`}</h2>
+                        <div className="flex items-center justify-between w-full">
+                           <h2 className="card-title">{`Quiz ${index + 1}: ${question}`}</h2>
+                           <button onClick={() => visibilityHandler(correctAnswer)}>
+                              <FontAwesomeIcon className="mx-4 cursor-pointer text-lg mt-1" icon={faEye}></FontAwesomeIcon>
+                           </button>
+                        </div>
                         <div className="grid grid-cols-1 sm:grid-cols-2 gap-5 w-full justify-center mt-5">
                            {options.map((option, idx) => {
                               const generateId = `id${idx}${index}${id}`;
