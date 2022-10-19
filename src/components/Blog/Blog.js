ad658c465791574ebd36f2c3e739eccbc0b012a8
aminul <accounreate@gmail.com>
2022-10-20 01:01:11 +0600

    question2 added

--- src/components/Blog/Blog.js
+++ src/components/Blog/Blog.js
@@ -9,6 +9,12 @@ const Blog = () => {
                <p>ReactJS Router is mainly used for developing Single Page Web Applications. React Router is used to define multiple routes in the application. When a user types a specific URL into the browser, and if this URL path matches any 'route' inside the router file, the user will be redirected to that particular route.</p>
             </div>
          </div>
+         <div tabIndex={0} className="collapse collapse-arrow border-4 bg-base-100 rounded">
+            <div className="collapse-title text-xl font-medium">How does context api works?</div>
+            <div className="collapse-content font-semibold">
+               <p>The React Context API is a way for a React app to effectively produce global variables that can be passed around. This is the alternative to "prop drilling" or moving props from grandparent to child to parent, and so on. Context is also touted as an easier, lighter approach to state management using Redux.</p>
+            </div>
+         </div>
       </div>
    );
 };
